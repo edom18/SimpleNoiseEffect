44affb198d63ba05dad9761223c987af34e76645
edom18 <edo.m18@gmail.com>
2019-10-12 20:46:38 +0900

    feat: Added animation controls.

--- Assets/SimpleNoiseEffect/Scripts/Demo/ParticleDemo.cs
+++ Assets/SimpleNoiseEffect/Scripts/Demo/ParticleDemo.cs
@@ -17,8 +17,9 @@ namespace SNE
         [SerializeField]
         private MeshInfo[] _meshInfos = null;
 
-        private ParticleEffect _effect = null;
         private int _index = -1;
+        private ParticleEffect _effect = null;
+        private Coroutine _coroutine = null;
 
         private void Start()
         {
@@ -32,6 +33,29 @@ namespace SNE
             {
                 Next();
             }
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                AnimationFoward();
+            }
+
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                AnimationBack();
+            }
+        }
+
+        private void OnGUI()
+        {
+            if (GUI.Button(new Rect(50, 50, 150, 30), "Animation forward (F)"))
+            {
+                AnimationFoward();
+            }
+
+            if (GUI.Button(new Rect(50, 100, 150, 30), "Animation back (B)"))
+            {
+                AnimationBack();
+            }
         }
 
         private void Next()
@@ -43,6 +67,54 @@ namespace SNE
 
             _effect.SetMesh(mesh);
             _effect.SetTexture(tex);
+        }
+
+        private void AnimationFoward()
+        {
+            if (_coroutine != null)
+            {
+                StopCoroutine(_coroutine);
+            }
+
+            _coroutine = StartCoroutine(AnimationImpl(1f, false));
+        }
+
+        private void AnimationBack()
+        {
+            if (_coroutine != null)
+            {
+                StopCoroutine(_coroutine);
+            }
+
+            _coroutine = StartCoroutine(AnimationImpl(1f, true));
+        }
+
+        private IEnumerator AnimationImpl(float duration, bool reverse)
+        {
+            float time = 0;
+
+            while (true)
+            {
+                if (time >= duration)
+                {
+                    _effect.Progres(reverse ? 0 : 1f);
+                    break;
+                }
+
+                float t = time / duration;
+                t = t * t;
+
+                if (reverse)
+                {
+                    t = 1f - t;
+                }
+
+                _effect.Progres(t);
+
+                yield return null;
+
+                time += Time.deltaTime;
+            }
         }
     }
 }
